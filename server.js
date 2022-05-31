15d9fbb43c5cefd6a46b3203edd54ee10da0f9ff
VinVash <2020kucp1046@iiitkota.ac.in>
2022-06-01 01:37:05 +0530

    changed cors origins

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+  })
+);
 
 // routes
 app.use("/api/users", userRoutes);
