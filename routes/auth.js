a2e177f6ff39682f78ebf3e0e86d8d07a31650b1
VinVash <2020kucp1046@iiitkota.ac.in>
2022-06-01 23:09:03 +0530

    sending name with the request in login

--- routes/auth.js
+++ routes/auth.js
@@ -22,7 +22,13 @@ router.post("/", async (req, res) => {
       return res.status(401).send({ message: "Invalid email or password" });
 
     const token = user.generateAuthToken();
-    res.status(200).send({ data: token, message: "Logged in successfully" });
+    res
+      .status(200)
+      .send({
+        data: token,
+        name: user.firstName,
+        message: "Logged in successfully",
+      });
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
